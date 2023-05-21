32028f0c272b8cfcb65be7a65301e770a6847840
Shreeram <shreeram.shanmugasundaram_entc21@pccoer.in>
2023-05-21 23:00:30 +0530

    EMS Academy Upload System

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import "./App.css";
+import "./App.css";
 import FormMain from "./Pages/FormMain";
 import Nav from "./Components/Nav";
 import Shorts from "./Pages/Shorts";
